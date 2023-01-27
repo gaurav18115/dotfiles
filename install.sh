68f3acd3af64ac71cf15efaefe7c975fedd8711b
Gaurav Rao <gaurav.rao@avala.ai>
2023-01-27 14:24:49 +0530

    Replace comments with echo statements

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-
-# Setup Tmux
+echo "Installing tmux"
 sudo apt-get update 
 sudo apt-get install tmux -y
 
+echo "Downloading .tmux.conf"
 cd ~/
 wget https://github.com/gaurav18115/dotfiles/blob/main/.tmux.conf
 
-# Install oh-my-zsh
+echo "Installing oh-my-zsh"
 rm -rf ~/.oh-my-zsh
 yes | sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 zsh
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="afowler"/' ~/.zshrc
 
-# Install Chrome driver for rspec
+echo "Installing Chrome driver"
 sudo apt-get install chromium-driver -y
+
+echo "Installing firefox"
 sudo apt-get install firefox -y
